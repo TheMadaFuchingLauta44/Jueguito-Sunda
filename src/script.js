b2e7057a10958d7c713ef1a6f14b62f310669886
Vengeance <127500937+TheMadaFuchingLauta44@users.noreply.github.com>
2023-11-11 14:16:30 -0300

    los cambios estan explicados en la bitacora

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 var config= {
     type: Phaser.AUTO,
-    width: 832,
-    height: 645,
+    width: 800,
+    height: 600,
     physics: {
       default: 'arcade',
       arcade:{
@@ -15,7 +15,11 @@ var config= {
         create: create,
         update: update,
     }
-}
+};
+
+var score = 0;
+var scoreText;
+var gameOver = false;
 
 var game = new Phaser.Game(config);
 
@@ -24,25 +28,128 @@ function preload(){
   this.load.image('ground', 'assets/plat.png');
   this.load.image('star', 'assets/burga.png');
   this.load.image('bomb', 'assets/Mc.png');
-  this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 64});
+  this.load.image('ground2', 'assets/plat2.png');
+  this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
 
 }
 
 function create(){
-  this.add.image(416, 322, 'sky')
+  this.add.image(400, 300, 'sky')
 
   platforms = this.physics.add.staticGroup();
 
   platforms.create(425, 568, 'ground').setScale(3).refreshBody();
+  platforms.create(400, 400, 'ground')
+  platforms.create(850, 300, 'ground')
+  platforms.create(-50, 300, 'ground')
+  platforms.create(400, 200, 'ground2')
+  platforms.create(-50, 100, 'ground')
+  platforms.create(850, 100, 'ground')
 
-  platforms.create(50, 400, 'ground')
-  platforms.create(700, 300, 'ground')
-  platforms.create(50, 200, 'ground')
-  platforms.create(400, 50, 'ground')
 
   player = this.physics.add.sprite(100, 450, 'dude');
+  player.setCollideWorldBounds(true);
+  player.setBounce(0.2);
+  player.body.setGravityY(110);
 
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+  key: 'turn',
+    frames: [{key: 'dude', frame: 4}],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.physics.add.collider(player, platforms);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: {x: 12, y:0, stepX: 70}
+  });
+
+  stars.children.iterate(function(child){
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+  });
+
+  this.physics.add.collider(stars, platforms);
+
+  this.physics.add.overlap(player, stars, collectStar, null, true);
+
+  scoreText = this.add.text(16, 16, 'Score: 0', {fontSize: '32px', fill: '#000'});
+
+   bombs = this.physics.add.group();
+
+   this.physics.add.collider(bombs, platforms);
+   this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
+
 function update(){
 
+if(gameOver){
+  return
+}
+
+  if(cursors.left.isDown){
+
+    player.setVelocityX(-160);
+    player.anims.play('left', true)
+  }
+  else if(cursors.right.isDown){
+
+    player.setVelocityX(160);
+    player.anims.play('right', true)
+}
+else{
+  player.setVelocityX(0);
+  player.anims.play('turn')
+}
+
+if(cursors.up.isDown && player.body.touching.down){
+  player.setVelocityY(-340);
+}
+
+}
+
+function collectStar(player, star) {
+  star.disableBody(true, true);
+  score += 5;
+  scoreText.setText('Score: ' + score);
+
+  if(stars.countActive(true) === 0){
+    stars.children.iterate(function(child){
+      child.enableBody(true, child.x, 0, true, true);
+  });
+ }
+
+  var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+  var bomb = bombs.create(x, 16, 'bomb');
+  bomb.setBounce(1);
+  bomb,setCollideWorldBounds(true);
+  bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
+
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause();
+  player.setTint(0xff0000);
+  player.anims.play('turn');
+  gameOver = true;
+  
 }
